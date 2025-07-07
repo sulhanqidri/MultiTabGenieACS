a6d9a332c12861c8f8f7e26b8a9798ce0087b59d
Sulhan Qidri <sulhanqidri@gmail.com>
2025-07-07 16:10:43 +0700

    validasi

--- install.sh
+++ install.sh
@@ -4,54 +4,55 @@ RED='\033[0;31m'
 NC='\033[0m'
 local_ip=$(hostname -I | awk '{print $1}')
 echo -e "${GREEN}============================================================================${NC}"
-echo -e "${GREEN}==================== Script Install  MultoTab GenieACS. ====================${NC}"
+echo -e "${GREEN}==================== Script Install  MultiTab GenieACS. ====================${NC}"
 echo -e "${GREEN}===================== By LSTNetwork. Info 085322692888 =====================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
 
 # Install git
 apt install git -y
 
-# Clone Git
-git clone https://github.com/sulhanqidri/MultiTabGenieACS
+# Cek apakah folder sudah ada
+if [ -d "MultiTabGenieACS" ]; then
+    echo -e "${GREEN}Skip Clone...${NC}"
+else
+    git clone https://github.com/sulhanqidri/MultiTabGenieACS
+fi
 
 # Masuk folder
-cd MultiTabGenieACS
+cd MultiTabGenieACS || { echo -e "${RED}Gagal masuk folder MultiTabGenieACS. Pastikan folder ada.${NC}"; exit 1; }
 
 echo -e "${GREEN}Multi tab full parameter? (y/n)${NC}"
 read installtabfull
 if [ "$installtabfull" != "y" ]; then
-    # Repelace folder GenieACS
+    # Replace folder GenieACS
     cp -ru genieacs /usr/lib/node_modules/
     rm -r /usr/lib/node_modules/genieacs/public/app.css /usr/lib/node_modules/genieacs/public/logo.svg
     mv /usr/lib/node_modules/genieacs/public/app-ori.css /usr/lib/node_modules/genieacs/public/app.css
     mv /usr/lib/node_modules/genieacs/public/logo-ori.svg /usr/lib/node_modules/genieacs/public/logo.svg
-
 else
-    # Repelace folder GenieACS
+    # Replace folder GenieACS
     cp -ru genieacs /usr/lib/node_modules/
-
     rm -r /usr/lib/node_modules/genieacs/public/app-ori.css /usr/lib/node_modules/genieacs/public/logo-ori.svg
 
-    # Restor Database GenieACS
+    # Restore Database GenieACS
     mongorestore --db genieacs --drop database
 fi
 
-
-# Keluar 1 Folder
+# Keluar satu folder
 cd ..
 
 # Hapus Folder
 rm -r AutoInstallGenieACS/ MultiTabGenieACS/
 
-#Sukses
+# Sukses
 echo -e "${GREEN}============================================================================${NC}"
 echo -e "${GREEN}========== GenieACS UI akses port 3000. : http://$local_ip:3000 ============${NC}"
 echo -e "${GREEN}=================== Informasi: Whatsapp 0853-2269-2888 =====================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
-#Restart
 
+# Restart Countdown
 for ((i = 3; i >= 1; i--)); do
-	sleep 1
+    sleep 1
     echo "Menghitung $i. Tekan ctrl+c untuk membatalkan Restart"
 done
 
